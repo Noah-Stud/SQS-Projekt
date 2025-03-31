ed30ee04d34bdfe171e48db224ec684dfbb79e1f
studheupno4892 <noah.heupel@stud.th-rosenheim.de>
2025-03-31 23:31:08 +0200

    Changed Filename

--- sqsbackend/src/main/java/com/studheupno/sqsbackend/SqsBackendApplication.java
+++ sqsbackend/src/main/java/com/studheupno/sqsbackend/SqsBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqsbackendApplication {
+public class SqsBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqsbackendApplication.class, args);
+		SpringApplication.run(SqsBackendApplication.class, args);
 	}
 
 }
