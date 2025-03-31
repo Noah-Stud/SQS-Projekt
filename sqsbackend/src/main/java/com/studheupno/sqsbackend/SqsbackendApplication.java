4ad9dd1f7d638e94f0e878e4229973978e9a76c5
Noah-Stud <131972699+Noah-Stud@users.noreply.github.com>
2025-03-31 23:04:21 +0200

    Add Springboot for Backend

---
+++ sqsbackend/src/main/java/com/studheupno/sqsbackend/SqsbackendApplication.java
@@ -0,0 +1,13 @@
+package com.studheupno.sqsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqsbackendApplication.class, args);
+	}
+
+}
---
+++ sqsbackend/src/test/java/com/studheupno/sqsbackend/SqsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studheupno.sqsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
