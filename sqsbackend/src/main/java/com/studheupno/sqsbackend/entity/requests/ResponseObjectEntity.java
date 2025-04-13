3a946480d9f557478b4bc01f2085e857de302d22
studheupno4892 <noah.heupel@stud.th-rosenheim.de>
2025-04-13 12:59:37 +0200

    added first tests and mockito dependencie

--- sqsbackend/src/main/java/com/studheupno/sqsbackend/entity/requests/ResponseObjectEntity.java
+++ sqsbackend/src/main/java/com/studheupno/sqsbackend/entity/requests/ResponseObjectEntity.java
@@ -1,8 +1,10 @@
 package com.studheupno.sqsbackend.entity.requests;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+@Data
 @Setter
 @Getter
 public class ResponseObjectEntity {
