7bf553570cc33285f600c8a0d807d0d8a5a05956
studheupno4892 <noah.heupel@stud.th-rosenheim.de>
2025-04-18 15:45:13 +0200

    added ArchUnitTests

---
+++ sqsbackend/src/test/java/com/studheupno/sqsbackend/ArchUnitTests.java
@@ -0,0 +1,43 @@
+package com.studheupno.sqsbackend;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+public class ArchUnitTests {
+    @Test
+    public void annotationNames() {
+        JavaClasses importedClasses = new ClassFileImporter().withImportOption(new ImportOption.DoNotIncludeTests()).importPackages("com.studheupno.sqsbackend");
+
+        ArchRule rule = classes().that().areAnnotatedWith(Service.class).should().haveSimpleNameEndingWith("Service");
+        rule.check(importedClasses);
+
+        rule = classes().that().areAnnotatedWith(RestController.class).should().haveSimpleNameEndingWith("Controller");
+        rule.check(importedClasses);
+
+        rule = classes().that().areAnnotatedWith(Configuration.class).should().haveSimpleNameEndingWith("Config");
+        rule.check(importedClasses);
+
+        rule = classes().that().areAnnotatedWith(Repository.class).should().haveSimpleNameEndingWith("Repo");
+        rule.check(importedClasses);
+    }
+
+    @Test
+    public void packageContainmentTest () {
+        JavaClasses importedClasses = new ClassFileImporter().withImportOption(new ImportOption.DoNotIncludeTests()).importPackages("com.studheupno.sqsbackend");
+
+        ArchRule rule = classes().that().areNotAnnotations().and().haveSimpleNameEndingWith("Service").should().resideInAPackage("..service");
+        rule.check(importedClasses);
+
+        rule = classes().that().areNotAnnotations().and().haveSimpleNameEndingWith("Entity").should().resideInAPackage("..entity..");
+        rule.check(importedClasses);
+    }
+}
