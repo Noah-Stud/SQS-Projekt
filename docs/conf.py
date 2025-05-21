6feb0df40057867816250b0f1377fb503f5f455c
studheupno4892 <noah.heupel@stud.th-rosenheim.de>
2025-05-21 18:07:25 +0200

    ReadTheDoc added

--- docs/conf.py
+++ docs/conf.py
@@ -5,16 +5,6 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
 # -- Project information -----------------------------------------------------
 
 project = "Basic Sphinx Example Project"
